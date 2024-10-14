5ec510fa5b6b2196c431f8152aa1b999020e16af
marcushock <m1hock@ucsd.edu>
2024-10-14 08:21:53 -0700

    Cleaned up the helper funcrtions, and added additional features.
    Specifically, fixed error handling in the extraction
    of parameters. More importantly, created the
    steady state dataframe construction. Note that
    the pCa decimals make it tricky to index correctly.
    Should be fairly reliable, but may have issues.

--- resultsOrg/helper_functions.py
+++ resultsOrg/helper_functions.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 
 class states_instance:
-    def __init__(self, input_filename:'CSV') -> None:
+    def __init__(self, input_filename:'CSV') -> None: # type: ignore
         print('The filename is: ', input_filename)
         self.file_name = input_filename
         self.read_states()
         self.extract_parameters()
+        self.states_steadystate()
         return 
     
     def read_states(self):
@@ -32,11 +33,42 @@ class states_instance:
             except:
                 try:
                     print('Flipping order for parameters')
+                    self.parameters[params_list[i+1]] = float(params_list[i])
                 except:
-                    print('Failed to make dict')
-                    return 
+                    print('Failed to make with values {} and {}'.format(params_list[i], params_list[i+1]))
+                    i +=1
+                    next 
             i += 2
-        print('Read in dict', self.parameters)
+        print('Read in parameters', self.parameters)
         return 
 
-states_instance('/crucial/temp_MCMC/dATP_multiscale_modeling/MCMC_simulation_results/241003-1020_States_out k2_plus_ref 0.004780 k3_plus 0.050000 k4_plus_ref 0.135000 kB_plus_ref 13.000000 kB_minus_ref 0.100000 kCa_plus_ref 0.090000 dATP 0.050000 k_force 0.200000 k_plus_SR_ref 16.000000 k_minus_SR_ref 15.000000.csv')
+
+    def states_steadystate(self, end_time_amount= 500): 
+        temp_series = self.states_df[self.states_df.Time>end_time_amount].mean()
+        # print(temp_series)
+        # Define new steady state DF with each column a different state
+        # And each row a different pCa (7,4,0.1 steap)
+        # Also creating a place for the pCa first, then renaming 
+        df = pd.DataFrame(np.zeros((31,6)), columns = ['pCa','M2','M1','C','B','OFF'])
+        df.pCa = np.arange(7,3.9,-0.1)
+        for i in range(len(df.pCa)):
+            df.pCa[i] = round(df.pCa[i],1)
+        df = df.set_index('pCa')
+
+        # Annoying rounding necessary for the python decimal storage necessity
+        pca = 7
+        while pca >= 4:
+            for state in ['M2','M1','C','B','OFF']:
+                value = temp_series.get(f'{state} {pca:.1f}')
+                print(value)
+                df.at[round(pca,2), state] = value 
+            pca -= 0.1
+
+        self.steady_states_all  = df
+        self.force_pCa = self.steady_states_all['M2']
+
+
+
+
+my_var = states_instance('/crucial/temp_MCMC/dATP_multiscale_modeling/MCMC_simulation_results/241004-1555_MR_640_States_out k2_plus_ref 0.002500 k3_plus 0.050000 k4_plus_ref 0.135000 kB_plus_ref 13.000000 kB_minus_ref 0.100000 kCa_plus_ref 0.090000 dATP 0.250000 k_force 0.000200 k_plus_SR_ref 16.000000 k_minus_SR_ref 15.000000.csv')
+

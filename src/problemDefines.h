2923ae32381f25db7518db999e1496da9d621cd7
marcushock <m1hock@ucsd.edu>
2025-08-12 14:58:34 -0700

    Changed values back to the traditionally used 15e-4f and 1500001 steps

--- src/problemDefines.h
+++ src/problemDefines.h
@@ -4,8 +4,8 @@
 #define N_RU 26 // Number of RUs // parameter defined here
 #define N_S 7 // Number of states
 #define MAX_REPS (400) // Max number used to repeat the simulation 32 blocks * 32 threads //150016
-#define DT 20e-4f // fixed time step
-#define MAX_TSTEPS (30000001) // Max number of time stepping 3 seconds
+#define DT 15e-4f // fixed time step
+#define MAX_TSTEPS (1500001) // Max number of time stepping 3 seconds
 #define SEED (time(NULL))
 #define RANDVAL (rand())
 #endif // PROBLEM_DEFINES_H
